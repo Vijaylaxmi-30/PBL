224156fbf536f9c9dac93e94f5f7659963f3caf5
sarveshwani0501 <smwsarvesh@gmail.com>
2025-02-05 10:10:52 +0530

    home page added

---
+++ homepage.js
@@ -0,0 +1,10 @@
+let loginId = document.querySelector(".login");
+
+loginId.addEventListener("click", () => {
+  window.location.href = "login.html";
+});
+
+let signUpId = document.querySelector(".signup");
+signUpId.addEventListener("click", () => {
+  window.location.href = "signUp.html";
+});
